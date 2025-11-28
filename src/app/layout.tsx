ee7d3dff2a2c58ddd1b4ea15f220148803506968
Guilherme Moreira <guilhermemoreira.web@gmail.com>
2025-11-28 13:36:44 -0300

    adjustment in login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,30 @@ import "./globals.css";
 import Link from "next/link";
 import type { ReactNode } from "react";
 
-export const metadata = { title: "Helena CRM Starter", description: "Next.js + Postgres + Auth + Tailwind" };
+export const metadata = {
+  title: "Helena CRM Starter",
+  description: "Next.js + Postgres + Auth + Tailwind",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning={true} data-lt-installed="true">
       <body className="w-full">
         <header className="border-b bg-white">
           <div className="flex items-center justify-between py-3 mx-8">
-            <Link href="/" className="font-semibold">CRM</Link>
+            <Link href="/" className="font-semibold">
+              CRM
+            </Link>
             <nav className="space-x-4">
-              <Link className="link" href="/signin">Entrar</Link>
-              <Link className="link" href="/signup">Criar conta</Link>
-              <Link className="link" href="/dashboard">Dashboard</Link>
+              <Link className="link" href="/signin">
+                Entrar
+              </Link>
+              <Link className="link" href="/signup">
+                Criar conta
+              </Link>
+              <Link className="link" href="/dashboard">
+                Dashboard
+              </Link>
             </nav>
           </div>
         </header>
